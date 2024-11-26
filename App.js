b2208a15e11958c420ead69d90e3c7fdb5410eff
Arthur Ferreira <arthur203teixeira@gmail.com>
2024-11-26 11:22:31 -0300

    main

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/screens/HomeScreen';
+import MonthDetailsScreen from './src/screens/MonthDetailsScreen';
+import SummaryScreen from './src/screens/SummaryScreen';
+import AddExpenseScreen from './src/screens/AddExpenseScreen';  // Adicionando o import
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{ title: 'Controle de Finanças' }} 
+        />
+        <Stack.Screen 
+          name="MonthDetails" 
+          component={MonthDetailsScreen} 
+          options={{ title: 'Detalhes do Mês' }} 
+        />
+        <Stack.Screen 
+          name="Summary" 
+          component={SummaryScreen} 
+          options={{ title: 'Resumo do Mês' }} 
+        />
+        <Stack.Screen 
+          name="AddExpense"  // Nome da tela
+          component={AddExpenseScreen}  // Componente de adição de despesa
+          options={{ title: 'Adicionar Despesa' }} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
